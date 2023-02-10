e253149d5e22727c9220cc066bcf26662d6a17fb
Mahammadsalim Sema <msema5671@conestogac.on.ca>
2023-02-10 12:12:41 -0500

    Project Structure

---
+++ index.js
@@ -0,0 +1,41 @@
+//Importing the express server
+const express = require('express');
+const path = require('path');
+
+//Set up the inbuilt express validator for basic validation
+const {check, validationResult} = require('express-validator'); 
+
+//Import mongoose library
+const mongoose = require('mongoose');
+
+//connect to database
+//mongoose.connect('mongodb://127.0.0.1:27017/capstone_conestogahub');
+
+//Define the models
+
+
+//Create a express variable
+var myApp = express();
+
+//Using public folder for static files
+myApp.use(express.static(__dirname + '/public'));
+
+//Defining view engine and views
+myApp.set('view engine', 'ejs');
+myApp.set('views', path.join(__dirname, 'views'));
+
+//Set up the body parser
+myApp.use(express.urlencoded({extended:false}));
+
+//routes
+myApp.get('/', 
+    function (req, resp) {
+        resp.send('<h1>Welcome to my website ..</h1>'); 
+});
+
+
+//assigning the port
+myApp.listen(8080);
+
+//website home page
+console.log('Open http://localhost:8080/');
