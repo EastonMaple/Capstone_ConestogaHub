4e85d8b69dda0662d9bab6a3199491194324bdba
yunxiang-li-github <yli4830@conestogac.on.ca>
2023-02-14 09:56:05 -0500

    add register user validation

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route   GET users
-// @desc    Test route
+// @route   POST users
+// @desc    Register user
 // @access  Public
-router.get('/',(req,res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
