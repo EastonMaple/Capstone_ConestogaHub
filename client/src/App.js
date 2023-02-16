34850a044a66f70a20bb5ea0d6b0f582b39fb3bd
yunxiang-li-github <yli4830@conestogac.on.ca>
2023-02-16 14:44:57 -0500

    add React!

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import { Navbar } from './components/layout/Navbar';  
+import { Landing } from './components/layout/Landing';
+import './App.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Conestoga Hub</h1>
+          <p className="lead">
+            Create a profile/portfolio, share posts and get help from
+            other students
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">Sign Up</a>
+            <a href="login.html" className="btn btn-light">Login</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
