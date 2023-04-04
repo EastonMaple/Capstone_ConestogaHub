88fd648cd10ca5a0173b7a61bf147f557029a27a
Khaled <khaledhadida_@hotmail.com>
2023-04-04 19:05:23 -0400

    Added user profile search bar to help users find each other on platform

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,16 +1,26 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileItem from './ProfileItem';
 import { getProfiles } from '../../actions/profile';
 
-const Profiles = ({ getProfiles,setTitle, profile: { profiles, loading } }) => {
+const Profiles = ({ getProfiles, setTitle, profile: { profiles, loading } }) => {
   useEffect(() => {
     getProfiles();
     setTitle('Student Profiles'); // update the title when the component is mounted
   }, [getProfiles]);
-
+  //Usestate to store the User search
+  const [searchUser, setSearchUser] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Form submitted with value: ${profiles[0].user.name}`);
+    // Do something with the form data, such as send it to a server
+  };
+  //handle changes
+  const handleChange = (event) => {
+    setSearchUser(event.target.value);
+  };
   return (
     <section className="container">
       {loading ? (
@@ -18,13 +28,20 @@ const Profiles = ({ getProfiles,setTitle, profile: { profiles, loading } }) => {
       ) : (
         <Fragment>
           <h1 className="large text-primary">Students</h1>
+          <div className="search-container">
+            <form onSubmit={handleSubmit}>
+              <input type="text" placeholder="Search by name" value={searchUser} onChange={handleChange}/>
+                <button type="submit"><i style={{margin:"10px"}} className="fa fa-search"></i></button>
+            </form>
+          </div>
           <p className="lead">
             <i className="fab fa-connectdevelop" /> Browse and connect with
             students
           </p>
           <div className="profiles">
             {profiles.length > 0 ? (
-              profiles.map((profile) => (
+              profiles.filter((profile)=> searchUser ? profile.user.name.toLowerCase().includes(searchUser.toLowerCase()):true)
+              .map((profile) => (
                 <ProfileItem key={profile._id} profile={profile} />
               ))
             ) : (
