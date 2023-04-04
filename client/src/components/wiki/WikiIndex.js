29b9dc56e28d818169eb6354ad151a20bc9ecff8
yunxiang-li-github <yli4830@conestogac.on.ca>
2023-04-04 00:08:32 -0400

    fix a styling issue on wiki

--- client/src/components/wiki/WikiIndex.js
+++ client/src/components/wiki/WikiIndex.js
@@ -39,23 +39,12 @@ const WikiIndex = () => {
                   <Link to={wikiPagesDict['part time jobs']}>
                     <li>How to find part-time jobs</li>
                   </Link>
-                  <li>How to apply for study permit (and extension)</li>
                   <Link to={wikiPagesDict['work permit']}>
                     <li>How to apply for work permit</li>
                   </Link>
                   <Link to={wikiPagesDict['health insurance']}>
                     <li>Health Insurance</li>
                   </Link>
-                  <li>How to find your timetable</li>
-                  <li>How to find room for rent</li>
-                  <li>How to apply for G1 license</li>
-                  <li>Immigration Information for students</li>
-                  <li>Immigration information for family members</li>
-                  <li>Social insurance number for international students</li>
-                  <li>Introduction to conestoga college learning</li>
-                  <li>Familiarize new students with banking services</li>
-                  <li>Local and regional transportation</li>
-                  <li>Familiarize new students with mobile services</li>
                 </div>
               </div>
             </div>
